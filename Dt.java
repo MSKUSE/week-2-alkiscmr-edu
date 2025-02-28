de8a120fb05ac275f5a96177c925bd537284c530
alkiscmr-edu <cemrealkis@posta.mu.edu.tr>
2025-02-28 12:12:46 +0300

    Add files via upload

---
+++ Dt.java
@@ -0,0 +1,17 @@
+public class Dt {
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+
+            int grade = 1;
+            double grade2 = 1.5;
+            boolean isok = true;
+            char first = 'a';
+
+
+            System.out.println(grade);
+            System.out.println(grade2);
+            System.out.println(isok);
+            System.out.println(first);
+        }
+    }
